4f6d67187303cf1f979127e4011bd2fee56e2cfb
Mário Santos <3957598+gadelhas@users.noreply.github.com>
2024-05-16 15:22:24 +0100

    add wiggle

--- headline-block/build/index.asset.php
+++ headline-block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '933ae0f2083ac3aa2ea8');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'c4f5dd736a41a50d23d9');
