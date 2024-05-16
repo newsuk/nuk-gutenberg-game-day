7598ee1368aacfd04d48b856a663fc163327776a
benmalan <bmalandrinos@gmail.com>
2024-05-16 11:02:39 +0100

    added kicker to edit.js

---
+++ brilliantheadline/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'df9480129f547d388134');
---
+++ brilliantheadline/build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '6246405dd29efa010165');
