d0c2b868807f29219824047d3f089d3d93b766d3
Mário Santos <3957598+gadelhas@users.noreply.github.com>
2024-05-16 15:27:45 +0100

    Lingscars

--- headline-block/build/index.asset.php
+++ headline-block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'c4f5dd736a41a50d23d9');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '13eb6c8c07ce34ad684b');
