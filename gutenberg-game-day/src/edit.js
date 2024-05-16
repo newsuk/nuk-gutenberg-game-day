86744ed01a5b9a7e0206c1d7ec31fb1d2ab097a3
Malick Elgmati <malick.elgmati@news.co.uk>
2024-05-16 15:09:40 +0100

    Add support for 90s mode

--- gutenberg-game-day/src/edit.js
+++ gutenberg-game-day/src/edit.js
@@ -13,7 +13,7 @@ import { __ } from '@wordpress/i18n';
  */
 import { useState } from 'react';
 import { useBlockProps, RichText, InspectorControls, PanelColorSettings } from '@wordpress/block-editor';
-import { Notice } from '@wordpress/components';
+import { Notice, ToggleControl } from '@wordpress/components';
 
 /**
  * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
@@ -32,7 +32,7 @@ import './editor.scss';
  * @return {Element} Element to render.
  */
 export default function Edit( { attributes, setAttributes } ) {
-	const { kicker, headline, subdeck, headlineColour, kickerBackgroundColour } = attributes;
+	const { kicker, headline, subdeck, headlineColour, kickerBackgroundColour, is90sMode } = attributes;
 	const [showKickerWarning, setShowKickerWarning] = useState(false);
 	const [showHeadlineWarning, setShowHeadlineWarning] = useState(false);
 	const [showSubdeckWarning, setShowSubdeckWarning] = useState(false);
@@ -68,7 +68,10 @@ export default function Edit( { attributes, setAttributes } ) {
 	}
 
 	return (
-		<div { ...useBlockProps() }>
+		<div
+			{...useBlockProps()}
+			className={is90sMode ? 'nineties-mode' : ''}
+		>
 			<InspectorControls>
 				<PanelColorSettings
 					title={__('Kicker Background Colour', 'gutenberg-game-day')}
@@ -76,7 +79,7 @@ export default function Edit( { attributes, setAttributes } ) {
 						[
 							{
 								value: kickerBackgroundColour,
-								onChange: (newColour) => setAttributes({ kickerBackgroundColour: newColour }),
+								onChange: (newColour) => setAttributes({kickerBackgroundColour: newColour}),
 								label: __('Kicker Background Colour', 'gutenberg-game-day')
 
 							}
@@ -86,58 +89,80 @@ export default function Edit( { attributes, setAttributes } ) {
 				<PanelColorSettings
 					title={__('Headline Colour', 'gutenberg-game-day')}
 					colorSettings={
-					[
-						{
-							value: headlineColour,
-							onChange: (newColour) => setAttributes({ headlineColour: newColour }),
-							label: __('Headline Colour', 'gutenberg-game-day')
+						[
+							{
+								value: headlineColour,
+								onChange: (newColour) => setAttributes({headlineColour: newColour}),
+								label: __('Headline Colour', 'gutenberg-game-day')
 
-						}
-					]
+							}
+						]
 					}
 				/>
+				<ToggleControl
+					label="90s Mode"
+					checked={is90sMode}
+					onChange={(newIs90sMode) => setAttributes({is90sMode: newIs90sMode})}
+					className={'gutenberg-game-day-nineties-toggle'}
+				/>
 			</InspectorControls>
-			{ showKickerWarning && (
-				<Notice status="warning" isDismissible={false}>
-					{ __( 'The kicker cannot exceed 20 characters.', 'gutenberg-game-day' ) }
-				</Notice>
-			) }
-			<RichText
-				id={'gutenberg-game-day-kicker'}
-				className={'gutenberg-game-day-richtext'}
-				tagName="h4"
-				value={ kicker }
-				onChange={ ( newKicker ) => setKicker(newKicker) }
-				placeholder={ __( 'Add a kicker', 'gutenberg-game-day' ) }
-				style={{backgroundColor: kickerBackgroundColour}}
-			/>
-			{ showHeadlineWarning && (
-				<Notice status="warning" isDismissible={false}>
-					{ __( 'The headline cannot exceed 80 characters.', 'gutenberg-game-day' ) }
-				</Notice>
-			) }
-			<RichText
-				id={'gutenberg-game-day-headline'}
-				className={'gutenberg-game-day-richtext'}
-				tagName="h2"
-				value={ headline }
-				onChange={ ( newHeadline ) => setHeadline(newHeadline) }
-				placeholder={ __( 'Add a headline', 'gutenberg-game-day' ) }
-				style={{color: headlineColour}}
-			/>
-			{ showSubdeckWarning && (
-				<Notice status="warning" isDismissible={false}>
-					{ __( 'The subdeck cannot exceed 150 characters.', 'gutenberg-game-day' ) }
-				</Notice>
-			) }
-			<RichText
-				id={'gutenberg-game-day-subdeck'}
-				className={'gutenberg-game-day-richtext'}
-				tagName="h3"
-				value={ subdeck }
-				onChange={ ( newSubdeck ) => setSubdeck(newSubdeck) }
-				placeholder={ __( 'Add a subdeck', 'gutenberg-game-day' ) }
-			/>
+			<div className={'wp-block-create-block-gutenberg-game-day'}>
+				{showKickerWarning && (
+					<Notice status="warning" isDismissible={false}>
+						{__('The kicker cannot exceed 20 characters.', 'gutenberg-game-day')}
+					</Notice>
+				)}
+				<RichText
+					id={'gutenberg-game-day-kicker'}
+					className={'gutenberg-game-day-richtext'}
+					tagName="h4"
+					value={kicker}
+					onChange={(newKicker) => setKicker(newKicker)}
+					placeholder={__('Add a kicker', 'gutenberg-game-day')}
+					style={{backgroundColor: kickerBackgroundColour}}
+				/>
+				{showHeadlineWarning && (
+					<Notice status="warning" isDismissible={false}>
+						{__('The headline cannot exceed 80 characters.', 'gutenberg-game-day')}
+					</Notice>
+				)}
+				<RichText
+					id={'gutenberg-game-day-headline'}
+					className={'gutenberg-game-day-richtext'}
+					tagName="h2"
+					value={headline}
+					onChange={(newHeadline) => setHeadline(newHeadline)}
+					placeholder={__('Add a headline', 'gutenberg-game-day')}
+					style={{color: headlineColour}}
+				/>
+				<div className={'gutenberg-game-day-faces-group'}>
+					<img
+						src={"https://www.staging-thesun.co.uk/wp-content/uploads/2024/05/Joel.png"}
+						className={'gutenberg-game-day-faces dvd-screensaver'}
+					/>
+					<img
+						src={"https://www.staging-thesun.co.uk/wp-content/uploads/2024/05/Umer.png"}
+						className={'gutenberg-game-day-faces dvd-screensaver'}
+					/>
+					<img
+						src={"https://www.staging-thesun.co.uk/wp-content/uploads/2024/05/Jack.png"}
+						className={'gutenberg-game-day-faces dvd-screensaver'}
+					/>
+				</div>
+				{showSubdeckWarning && (
+					<Notice status="warning" isDismissible={false}>
+						{__('The subdeck cannot exceed 150 characters.', 'gutenberg-game-day')}
+					</Notice>
+				)}
+				<RichText
+					id={'gutenberg-game-day-subdeck'}
+					className={'gutenberg-game-day-richtext'}
+					tagName="h3"
+					value={subdeck}
+					onChange={(newSubdeck) => setSubdeck(newSubdeck)}
+					placeholder={__('Add a subdeck', 'gutenberg-game-day')}
+				/>
+			</div>
 		</div>
 	);
 }
--- gutenberg-game-day/src/save.js
+++ gutenberg-game-day/src/save.js
@@ -16,30 +16,46 @@ import { useBlockProps, RichText } from '@wordpress/block-editor';
  * @return {Element} Element to render.
  */
 export default function save({ attributes }) {
- const { kicker, headline, subdeck, headlineColour, kickerBackgroundColour } = attributes;
+ const { kicker, headline, subdeck, headlineColour, kickerBackgroundColour, is90sMode } = attributes;
 
  return (
-  <div { ...useBlockProps.save() }>
-     <RichText.Content
-      tagName="h4"
-      value={kicker}
-      style={{backgroundColor: kickerBackgroundColour}}
-	  id={'gutenberg-game-day-kicker'}
-	  className={'gutenberg-game-day-richtext'}
-     />
-     <RichText.Content
-      tagName="h2"
-      value={headline}
-      style={{color: headlineColour}}
-	  id={'gutenberg-game-day-headline'}
-	  className={'gutenberg-game-day-richtext'}
-     />
-     <RichText.Content
-      tagName="h3"
-      value={subdeck}
-	  id={'gutenberg-game-day-subdeck'}
-	  className={'gutenberg-game-day-richtext'}
-     />
+  <div { ...useBlockProps.save() } className={ is90sMode ? 'nineties-mode' : '' }>
+		  <div className={'wp-block-create-block-gutenberg-game-day'}>
+			  <RichText.Content
+				  tagName="h4"
+				  value={kicker}
+				  style={{backgroundColor: kickerBackgroundColour}}
+				  id={'gutenberg-game-day-kicker'}
+				  className={'gutenberg-game-day-richtext'}
+			  />
+			  <RichText.Content
+				  tagName="h2"
+				  value={headline}
+				  style={{color: headlineColour}}
+				  id={'gutenberg-game-day-headline'}
+				  className={'gutenberg-game-day-richtext'}
+			  />
+			  <div className={'gutenberg-game-day-faces-group'}>
+				  <img
+					  src={"https://www.staging-thesun.co.uk/wp-content/uploads/2024/05/Joel.png"}
+					  className={'gutenberg-game-day-faces dvd-screensaver'}
+				  />
+				  <img
+					  src={"https://www.staging-thesun.co.uk/wp-content/uploads/2024/05/Umer.png"}
+					  className={'gutenberg-game-day-faces dvd-screensaver'}
+				  />
+				  <img
+					  src={"https://www.staging-thesun.co.uk/wp-content/uploads/2024/05/Jack.png"}
+					  className={'gutenberg-game-day-faces dvd-screensaver'}
+				  />
+			  </div>
+			  <RichText.Content
+				  tagName="h3"
+				  value={subdeck}
+				  id={'gutenberg-game-day-subdeck'}
+				  className={'gutenberg-game-day-richtext'}
+			  />
+		  </div>
   </div>
  );
 }
