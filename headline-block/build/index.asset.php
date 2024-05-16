6d90002566f10dd1660383de24b5b0bca357db88
Dan Harling <dharling101@gmail.com>
2024-05-16 15:05:57 +0100

    fix: subdeck text color

--- headline-block/build/index.asset.php
+++ headline-block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'd22c08917fb7fcbd5613');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '78c182ee6cfebeeba92e');
