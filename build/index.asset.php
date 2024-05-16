a92d7d642cf7d41089ad71dc59a2fc7eeb218e21
ejonas-github <edwil.jonas@news.co.uk>
2024-05-16 17:15:27 +0200

    edwil-prem: Some styling updates

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'ae0d720f4a977f3928e0');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'f9e0f9d4dd9bd97edd25');
