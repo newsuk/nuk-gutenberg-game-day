581c4426edcb471fc57dbc04cfa24a5e08fb506d
preslavvpetkov <preslav.v.petkov@gmail.com>
2024-05-16 13:01:41 +0300

    adding create-block@latest

---
+++ funky-block/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'b57769b8cc468bc76e51');
---
+++ funky-block/build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '0348baf95a959ce6b1cc');
