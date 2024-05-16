019d07e1203b0cedef73057011d74974db286d9c
Prem Tiwari <prem.tiwari@news.co.uk>
2024-05-16 18:07:10 +0530

    Eye Catcher: Make eyecatcher block as default

--- eyecatcher.php
+++ eyecatcher.php
@@ -15,6 +15,9 @@ function load_eye_catcher() {
 
 add_action( "init", "load_eye_catcher" );
 
+/**
+ * Make eyecatcher block as default.
+ */
 function eyecatcher_register_template() {
     $post_type_object = get_post_type_object( 'post' );
     $post_type_object->template = array(
