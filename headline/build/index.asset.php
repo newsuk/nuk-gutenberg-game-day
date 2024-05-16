897e5cac0576f298133c9a5af29109961a7fd520
tsnow-uk <timothy.snow@news.co.uk>
2024-05-16 10:55:51 +0100

    initial commit

---
+++ headline/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'e093fcd7a2f439c44066');
---
+++ headline/build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'e7ba2b6d4e84cad48dad');
