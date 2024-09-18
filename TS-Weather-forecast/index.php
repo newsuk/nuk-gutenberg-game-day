142e05baded9651f63fa9602c7e5a9614d24e159
Arun Sharma <arun.sharma@news.co.uk>
2024-09-18 19:46:47 +0530

    indeting code for better visibility

--- TS-Weather-forecast/index.php
+++ TS-Weather-forecast/index.php
@@ -13,24 +13,24 @@ define( 'TS_WEATHER_FORECAST_BLOCK_VERSION', '1.0.0' );
 
 // Register the block
 function ts_weather_forecast_register_block() {
-    wp_register_script(
-        'ts-weather-forecast-block',
-        plugins_url( 'dist/block.js', __FILE__ ),
-        array( 'wp-blocks', 'wp-element', 'wp-editor' ),
-        TS_WEATHER_FORECAST_BLOCK_VERSION
-    );
+	wp_register_script(
+		'ts-weather-forecast-block',
+		plugins_url( 'dist/block.js', __FILE__ ),
+		array( 'wp-blocks', 'wp-element', 'wp-editor' ),
+		TS_WEATHER_FORECAST_BLOCK_VERSION
+	);
 
-    wp_register_style(
-        'ts-weather-forecast-block-style',
-        plugins_url( 'src/style.css', __FILE__ ),
-        array(),
-        TS_WEATHER_FORECAST_BLOCK_VERSION
-    );
+	wp_register_style(
+		'ts-weather-forecast-block-style',
+		plugins_url( 'src/style.css', __FILE__ ),
+		array(),
+		TS_WEATHER_FORECAST_BLOCK_VERSION
+	);
 
-    register_block_type('ts-weather-forecast/block', array(
-        'editor_script' => 'ts-weather-forecast-block',
-        'style' => 'ts-weather-forecast-block-style',
-    ));
+	register_block_type('ts-weather-forecast/block', array(
+		'editor_script' => 'ts-weather-forecast-block',
+		'style' => 'ts-weather-forecast-block-style',
+	));
 }
 add_action('init', 'ts_weather_forecast_register_block');
 
@@ -40,11 +40,11 @@ add_action('init', 'ts_weather_forecast_register_block');
  * @return void
  */
 function ts_weather_forecast_api() {
-    register_rest_route('ts-weather-forecast/v1', '/weather', array(
-        'methods' => 'GET',
-        'callback' => 'ts_weather_forecast_get_weather',
-        'permission_callback' => '__return_true',
-    ));
+	register_rest_route('ts-weather-forecast/v1', '/weather', array(
+		'methods' => 'GET',
+		'callback' => 'ts_weather_forecast_get_weather',
+		'permission_callback' => '__return_true',
+	));
 }
 add_action('rest_api_init', 'ts_weather_forecast_api');
 
@@ -57,30 +57,30 @@ add_action('rest_api_init', 'ts_weather_forecast_api');
  * or a WP_Error object if the API request fails.
  */
 function ts_weather_forecast_get_weather( $request ) {
-    $city = sanitize_text_field( $request->get_param( 'city' ) );
-    $date = sanitize_text_field( $request->get_param( 'date' ) );
-    $api_key = '54f9e00fb0c4728472a7cef3475ae9d2';
+	$city = sanitize_text_field( $request->get_param( 'city' ) );
+	$date = sanitize_text_field( $request->get_param( 'date' ) );
+	$api_key = '54f9e00fb0c4728472a7cef3475ae9d2';
 
-    // Convert date to timestamp
-    $timestamp = strtotime( $date );
+	// Convert date to timestamp
+	$timestamp = strtotime( $date );
 
-    // Use the timestamp to get the weather forecast for the specific date
-    $response = wp_remote_get( "http://api.openweathermap.org/data/2.5/forecast?q={$city}&appid={$api_key}&units=metric" );
+	// Use the timestamp to get the weather forecast for the specific date
+	$response = wp_remote_get( "http://api.openweathermap.org/data/2.5/forecast?q={$city}&appid={$api_key}&units=metric" );
 
-    if ( is_wp_error( $response ) ) {
-        return new WP_Error( 'api_error', 'Unable to fetch weather data', array( 'status' => 500 ) );
-    }
+	if ( is_wp_error( $response ) ) {
+		return new WP_Error( 'api_error', 'Unable to fetch weather data', array( 'status' => 500 ) );
+	}
 
-    $body = wp_remote_retrieve_body( $response );
-    $data = json_decode( $body, true );
+	$body = wp_remote_retrieve_body( $response );
+	$data = json_decode( $body, true );
 
-    // Find the forecast closest to the specified date
-    $closest_forecast = null;
-    foreach ( $data['list'] as $forecast ) {
-        if ( abs( $forecast['dt'] - $timestamp ) < abs( $closest_forecast['dt'] - $timestamp ) ) {
-            $closest_forecast = $forecast;
-        }
-    }
+	// Find the forecast closest to the specified date
+	$closest_forecast = null;
+	foreach ( $data['list'] as $forecast ) {
+		if ( abs( $forecast['dt'] - $timestamp ) < abs( $closest_forecast['dt'] - $timestamp ) ) {
+			$closest_forecast = $forecast;
+		}
+	}
 
-    return $closest_forecast;
+	return $closest_forecast;
 }
--- TS-Weather-forecast/webpack.config.js
+++ TS-Weather-forecast/webpack.config.js
@@ -1,23 +1,23 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/block.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'block.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@wordpress/babel-preset-default']
-                    }
-                }
-            }
-        ]
-    }
+	entry: './src/block.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'block.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@wordpress/babel-preset-default']
+					}
+				}
+			}
+		]
+	}
 };
