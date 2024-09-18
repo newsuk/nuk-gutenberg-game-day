8c03171bf7770e8a582bf5ed4d473978ea1559cd
Krisko <chrizkomovies93@gmail.com>
2024-09-18 15:07:10 +0300

    initial commit

--- fancy-headline/src/edit.js
+++ fancy-headline/src/edit.js
@@ -1,77 +1,128 @@
-import {InspectorControls, PanelColorSettings, useBlockProps} from '@wordpress/block-editor';
+import { InspectorControls, PanelColorSettings, useBlockProps } from '@wordpress/block-editor';
 import { RichText } from "@wordpress/block-editor";
 import './editor.scss';
-import {__} from "@wordpress/i18n";
-import {useEffect} from "@wordpress/element";
-import {useDispatch} from "@wordpress/data";
+import { __ } from "@wordpress/i18n";
+import { useEffect } from "@wordpress/element";
+import { useDispatch } from "@wordpress/data";
 
-export default function Edit( { attributes, setAttributes } ) {
-	const { text, color, backgroundColor } = attributes;
+export default function Edit({ attributes, setAttributes }) {
+	const { text, color, backgroundColor, headline, headlineColor, headlineBackgroundColor, subdeck, subdeckColor, subdeckBackgroundColor } = attributes;
 
 	const KICKER_MAX_LENGTH = 20;
 
-	const style = {
+	const kickerStyle = {
 		color,
-		backgroundColor
-	}
+		backgroundColor,
+	};
+
+	const headlineStyle = {
+		color: headlineColor,
+		fontSize: '400%',
+		marginTop: '10px',
+	};
+
+
+	const subdeckStyle = {
+		color: subdeckColor,
+		fontSize: '150%',
+		marginTop: '10px',
+	};
 
 	const { createNotice } = useDispatch('core/notices');
 	const { lockPostSaving, unlockPostSaving } = useDispatch('core/editor');
 
 	useEffect(() => {
-		if(text.length < 3) {
+		if (text.length < 3) {
 			lockPostSaving();
 			createNotice(
-				'error', // Can be one of: success, info, warning, error.
-				'Kicker length is not right', // Text string to display.
-				{
-					isDismissible: true, // Whether the user can dismiss the notice.
-				}
+				'error',
+				'Kicker length is not right',
+				{ isDismissible: true }
 			);
 		}
 
-		if(text.length > KICKER_MAX_LENGTH) {
+		if (text.length > KICKER_MAX_LENGTH) {
 			createNotice(
-				'error', // Can be one of: success, info, warning, error.
-				'Kicker max length is exceeded', // Text string to display.
-				{
-					isDismissible: true, // Whether the user can dismiss the notice.
-				}
+				'error',
+				'Kicker max length is exceeded',
+				{ isDismissible: true }
 			);
-
 			lockPostSaving();
 		} else {
 			unlockPostSaving();
 		}
-
 	}, [text]);
 
 	return (
 		<>
 			<RichText
-				{...useBlockProps( { style })}
-				tagName="span"
+				{...useBlockProps({ style: kickerStyle })}
+				tagName="p"
 				value={text}
-				placeholder={ __( 'Kicker...' ) }
-				onChange={(value) => setAttributes({text: value})}
+				placeholder={__('Kicker...')}
+				onChange={(value) => setAttributes({ text: value })}
+			/>
+
+			<RichText
+				{...useBlockProps({ style: headlineStyle })}
+				tagName="p"
+				value={headline}
+				placeholder={__('Headline...')}
+				onChange={(value) => setAttributes({ headline: value })}
+			/>
+			
+			<RichText
+				{...useBlockProps({ style: subdeckStyle })}
+				tagName="h2"
+				value={subdeck}
+				placeholder={__('Subdeck...')}
+				onChange={(value) => setAttributes({ subdeck: value })}
 			/>
 
 			<InspectorControls>
 				<PanelColorSettings
 					__experimentalIsRenderedInSidebar
-					title={ __( 'Color' ) }
-					colorSettings={ [
+					title={__('Kicker Color Settings')}
+					colorSettings={[
 						{
 							value: color,
-							onChange: (value) => setAttributes({color: value}),
-							label: __( 'Text Color' ),
+							onChange: (value) => setAttributes({ color: value }),
+							label: __('Text Color'),
 						},
 						{
 							value: backgroundColor,
-							onChange: (value) => setAttributes({backgroundColor: value}),
-							label: __( 'Background Color' ),
+							onChange: (value) => setAttributes({ backgroundColor: value }),
+							label: __('Background Color'),
 						}
-					] }
+					]}
+				/>
+			</InspectorControls>
+
+			<InspectorControls>
+				<PanelColorSettings
+					__experimentalIsRenderedInSidebar
+					title={__('Headline Color Settings')}
+					colorSettings={[
+						{
+							value: headlineColor,
+							onChange: (value) => setAttributes({ headlineColor: value }),
+							label: __('Headline Text Color'),
+						},
+					]}
+				/>
+			</InspectorControls>
+
+			<InspectorControls>
+				<PanelColorSettings
+					__experimentalIsRenderedInSidebar
+					title={__('Subdeck Color Settings')}
+					colorSettings={[
+						{
+							value: subdeckColor,
+							onChange: (value) => setAttributes({ subdeckColor: value }),
+							label: __('Subdeck Text Color'),
+						},
+					]}
 				/>
 			</InspectorControls>
 		</>
--- fancy-headline/src/save.js
+++ fancy-headline/src/save.js
@@ -2,15 +2,47 @@ import { RichText } from "@wordpress/block-editor";
 import { useBlockProps } from '@wordpress/block-editor';
 
 export default function save({ attributes }) {
-	const { text, color, backgroundColor } = attributes;
+	const { text, color, backgroundColor, headline, headlineColor, headlineBackgroundColor, subdeck, subdeckColor, subdeckBackgroundColor } = attributes;
 
-	const style = {
+	const kickerStyle = {
 		color,
-		backgroundColor
-	}
+		backgroundColor,
+	};
 
+	const headlineStyle = {
+		color: headlineColor,
+		fontSize: '400%',
+		marginTop: '10px',
+	};
+
+	const subdeckStyle = {
+		color: subdeckColor,
+		fontSize: '150%',
+		marginTop: '10px',
+	};
 
 	return (
-		<RichText.Content { ...useBlockProps.save({ style }) } value={text} tagName="span" />
+		<>
+			{/* Kicker */}
+			<RichText.Content
+				{...useBlockProps.save({ style: kickerStyle })}
+				value={text}
+				tagName="p"
+			/>
+
+			{/* Headline */}
+			<RichText.Content
+				{...useBlockProps.save({ style: headlineStyle })}
+				value={headline}
+				tagName="p"
+			/>
+
+			{/* Subdeck */}
+			<RichText.Content
+				{...useBlockProps.save({ style: subdeckStyle })}
+				value={subdeck}
+				tagName="h2"
+			/>
+		</>
 	);
 }
