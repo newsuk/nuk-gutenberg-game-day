31a95f78c82e19dd864f3d7b6b889b8a57e0eb76
Chris Kent <christopher.kent@news.co.uk>
2024-09-18 12:12:04 +0100

    got something working

--- src/edit.js
+++ src/edit.js
@@ -20,7 +20,10 @@ import { useBlockProps } from '@wordpress/block-editor';
  * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
  */
 import './editor.scss';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Doughnut } from 'react-chartjs-2';
 
+ChartJS.register(ArcElement, Tooltip, Legend);
 /**
  * The edit function describes the structure of your block in the context of the
  * editor. This represents what the editor will render when the block is used.
@@ -29,10 +32,45 @@ import './editor.scss';
  *
  * @return {Element} Element to render.
  */
+export const chartData = {
+	labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+	datasets: [
+	  {
+		label: '# of Votes',
+		data: [12, 19, 3, 5, 2, 3],
+		backgroundColor: [
+		  'rgba(255, 99, 132, 0.2)',
+		  'rgba(54, 162, 235, 0.2)',
+		  'rgba(255, 206, 86, 0.2)',
+		  'rgba(75, 192, 192, 0.2)',
+		  'rgba(153, 102, 255, 0.2)',
+		  'rgba(255, 159, 64, 0.2)',
+		],
+		borderColor: [
+		  'rgba(255, 99, 132, 1)',
+		  'rgba(54, 162, 235, 1)',
+		  'rgba(255, 206, 86, 1)',
+		  'rgba(75, 192, 192, 1)',
+		  'rgba(153, 102, 255, 1)',
+		  'rgba(255, 159, 64, 1)',
+		],
+		borderWidth: 1,
+	  },
+	],
+  };
+const chartOptions = {
+	plugins: {
+		legend: {
+			display: false
+		}
+	}
+}
+
 export default function Edit() {
 	return (
 		<p { ...useBlockProps() }>
 			{ __( 'Plotpoint – hello from the editor!', 'plotpoint' ) }
+			<Doughnut data={ chartData } options={ chartOptions } />
 		</p>
 	);
 }
