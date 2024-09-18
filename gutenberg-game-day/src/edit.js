b066ca20b960f418b92b82ea385aed0b4afe990d
Shadi Sharaf <shadi.sharaf@news.co.uk>
2024-09-18 14:41:11 +0300

    Move code to sidebar plugin

--- gutenberg-game-day/src/edit.js
+++ gutenberg-game-day/src/edit.js
@@ -11,14 +11,10 @@ import { __ } from "@wordpress/i18n";
  *
  * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-block-editor/#useblockprops
  */
-import { useState, useEffect } from "react";
-import apiFetch from "@wordpress/api-fetch";
-import { useBlockProps, InspectorControls } from "@wordpress/block-editor";
+import { useBlockProps } from "@wordpress/block-editor";
 import {
-	Draggable,
 	DropZone,
-	TextControl,
-	PanelBody,
+	DropZoneProvider,
 } from "@wordpress/components";
 
 /**
@@ -38,83 +34,19 @@ import "./editor.scss";
  * @return {Element} Element to render.
  */
 export default function Edit() {
-	const [posts, setPosts] = useState(null);
-	const [loading, setLoading] = useState(true);
-	const [draggedPost, setDraggedPost] = useState(null);
-
-	useEffect(() => {
-		const fetchPosts = async () => {
-			const posts = await apiFetch({ path: "/wp/v2/posts" });
-			console.log(posts);
-			setPosts(posts);
-			setLoading(false);
-		};
-
-		posts === null && fetchPosts();
-	});
-
-	// onDraggableStart = (event) => {
-	// 	console.log("drag start", event);
-	// };
-
-	// onDraggableEnd = (event, item) => {
-	// 	console.log("drag end", event);
-	// 	console.log("drag end item", item);
-	// };
-
 	return (
 		<>
-			<p {...useBlockProps()}>
-				{__(
-					"Gutenberg Game Day – hello from the editor!",
-					"gutenberg-game-day",
-				)}
-				<DropZone
-					onDrop={function noRefCheck(event, ref) {
-						console.log("dropped", event, ref);
-						console.log("draggedPost", draggedPost);
-					}}
-				/>
-			</p>
-
-			<InspectorControls>
-				<PanelBody title={__("Articles")}>
-					<TextControl
-						__nextHasNoMarginBottom
-						label="Search by article title"
-						// help="Additional help text"
-						// value={textField}
-						// onChange={onChangeTextField}
-					/>
-				</PanelBody>
-				<PanelBody>
-					{loading ? (
-						<p>Loading Posts...</p>
-					) : (
-						posts &&
-						posts.map((post) => (
-							<Draggable
-								key={post.id}
-								elementId={post.id}
-								transferData={{ ...post }}
-							>
-								{({ onDraggableStart, onDraggableEnd }) => (
-									<div
-										className="example-drag-handle"
-										draggable
-										onDragStart={function onDraggableStartEvent(event, ref) {
-											console.log("dropped", event, ref);
-										}}
-										// onDragEnd={onDraggableEnd}
-									>
-										<p key={post.id}>{post.title.rendered}</p>
-									</div>
-								)}
-							</Draggable>
-						))
+			<DropZoneProvider>
+				<p {...useBlockProps()}>
+					{__(
+						"Gutenberg Game Day – hello from the editor!",
+						"gutenberg-game-day",
 					)}
-				</PanelBody>
-			</InspectorControls>
+					<DropZone
+						onDrop={console.log}
+					/>
+				</p>
+			</DropZoneProvider>
 		</>
 	);
 }
--- gutenberg-game-day/src/sidebar/index.js
+++ gutenberg-game-day/src/sidebar/index.js
@@ -1,11 +1,81 @@
 import { registerPlugin } from '@wordpress/plugins';
 import { PluginSidebar } from '@wordpress/editor';
+import { __ } from "@wordpress/i18n";
+
+import { useState, useEffect } from "react";
+import apiFetch from "@wordpress/api-fetch";
+import {
+	Draggable,
+	TextControl,
+	PanelBody,
+} from "@wordpress/components";
 
 const Sidebar = () => {
+	const [posts, setPosts] = useState(null);
+	const [loading, setLoading] = useState(true);
+	const [draggedPost, setDraggedPost] = useState(null);
+
+	useEffect(() => {
+		const fetchPosts = async () => {
+			const posts = await apiFetch({ path: "/wp/v2/posts" });
+			console.log(posts);
+			setPosts(posts);
+			setLoading(false);
+		};
+
+		posts === null && fetchPosts();
+	});
+
+
+	const onDragStart = (event) => {
+		event.dataTransfer = new DataTransfer()
+		event.dataTransfer.setData("text/plain", 'test');
+		console.log( event.dataTransfer.getData("text") );
+	};
+
+	// onDraggableEnd = (event, item) => {
+	// 	console.log("drag end", event);
+	// 	console.log("drag end item", item);
+	// };
+
 	return (
-		<div>
-			<h1>Hello from the sidebar!</h1>
-		</div>
+		<>
+			<PanelBody title={__("Articles")}>
+				<TextControl
+					__nextHasNoMarginBottom
+					label="Search by article title"
+					// help="Additional help text"
+					// value={textField}
+					// onChange={onChangeTextField}
+				/>
+			</PanelBody>
+			<PanelBody>
+				{loading ? (
+					<p>Loading Posts...</p>
+				) : (
+					posts &&
+					posts.map((post) => (
+						<Draggable
+							key={post.id}
+							elementId={post.id}
+							transferData={{id: 1}}
+							appendToOwnerDocument={true}
+						>
+							{({ onDraggableStart, onDraggableEnd }) => (
+								<div
+									className="example-drag-handle"
+									draggable
+									onDragStart={console.log}
+									onDragEnd={console.log}
+								>
+									<p key={post.id}>{post.title.rendered}</p>
+								</div>
+							)}
+						</Draggable>
+					))
+				)}
+			</PanelBody>
+		</>
 	);
 };
 
