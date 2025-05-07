2301c5d6e125200aa7b59a55cf2b9b488ae68dc6
Jordan Clague <jordan.clague@news.co.uk>
2025-05-07 12:34:39 +0100

    tweaks

--- podcast/src/podcast/edit.js
+++ podcast/src/podcast/edit.js
@@ -11,8 +11,10 @@ import { __ } from '@wordpress/i18n';
  *
  * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-block-editor/#useblockprops
  */
-import { useBlockProps, InspectorControls } from '@wordpress/block-editor';
+import { useBlockProps, InspectorControls, RichText } from '@wordpress/block-editor';
 import { PanelBody, SelectControl, TextControl } from '@wordpress/components'
+import { useEffect, useState } from '@wordpress/element';
+import { podcasts } from './fixtures/podcastList';
 
 /**
  * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
@@ -33,10 +35,29 @@ import './editor.scss';
 export default function Edit({ attributes, setAttributes, id }) {
 	const { podcastSeries, episodeId, titleOverride, summaryOverride } = attributes;
 
+	let selectedPodcast = podcasts.find((podcast) => podcast.title === podcastSeries);
+
 	return (
 		<div {...useBlockProps()}>
-			<pre>{JSON.stringify(attributes, null, 2)}</pre>
+			{/* Edit view. */}
+			<h3>LATEST EPISODE</h3>
+			<RichText
+					tagName="h2" // The tag here is the element output and editable in the admin
+					value={titleOverride || podcastSeries} // Any existing content, either from the database or an attribute default
+					allowedFormats={[]} // Allow the content to be made bold or italic, but do not allow other formatting options
+					onChange={( newTitleOverride ) => setAttributes( { titleOverride: newTitleOverride } ) } // Store updated content as a block attribute
+					placeholder={ __( selectedPodcast.title ) } // Display this text before any content has been added by the user
+			/>
 
+			<RichText
+					tagName="p" // The tag here is the element output and editable in the admin
+					value={summaryOverride || selectedPodcast.metaDescription} // Any existing content, either from the database or an attribute default
+					allowedFormats={[]} // Allow the content to be made bold or italic, but do not allow other formatting options
+					onChange={( newSummaryOverride ) => setAttributes( { summaryOverride: newSummaryOverride } ) } // Store updated content as a block attribute
+					placeholder={ __( selectedPodcast.metaDescription ) } // Display this text before any content has been added by the user
+			/>
+
+			{/* Sidebar. */}
 			<InspectorControls>
 				<PanelBody title={ __( 'Podcast Settings', 'podcast' ) }>
 					<SelectControl
@@ -54,7 +75,11 @@ export default function Edit({ attributes, setAttributes, id }) {
 							{ value: "World in 10", label: "World in 10" },
 						] }
 						onChange={(newValue) => {
-							setAttributes({ podcastSeries: newValue });
+							setAttributes({
+								podcastSeries: newValue,
+								titleOverride: null,
+								summaryOverride: null,
+							});
 						}}
 						/>
 
